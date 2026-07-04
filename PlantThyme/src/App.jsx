ba0e32338e9d301e0cc080ac06557bc9aeb42f63
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-04 14:39:42 -0400

    Project Setup: new React App, removed boilerplate, installed react-router.

---
+++ PlantThyme/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div>This is the start of my Plant Thyme final project for LaunchCode!</div>
+        </>
+    );
+}
+
+export default App;
