345be5260235e11871e9715add1737fa8391750f
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-04 17:13:19 -0400

    Reorganize components into pages and layout folders.

--- PlantThyme/src/App.jsx
+++ PlantThyme/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
     return (
