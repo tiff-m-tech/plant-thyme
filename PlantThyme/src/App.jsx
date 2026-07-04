01dfada4b4debb80bc48d07cfa0c52850dfd0e73
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-04 16:51:14 -0400

    Add footer component and base styles.

--- PlantThyme/src/App.jsx
+++ PlantThyme/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
             <Header />
+            <main>Placeholder...</main>
+            <Footer />
         </>
     );
 }
---
+++ PlantThyme/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <span>© {new Date().getFullYear()} Plant Thyme</span>
+        </footer>
+    );
+}
+
+//
