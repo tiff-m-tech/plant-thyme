d5dc991dbaa3487b279089f2b626a23ab5b356c6
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-04 20:15:20 -0400

    Update class names in Home.jsx

--- PlantThyme/src/components/pages/Home.jsx
+++ PlantThyme/src/components/pages/Home.jsx
@@ -1,17 +1,15 @@
 import { altFromFileName } from "../../utils/altFromFileName";
+import { userName, leftDecoImagePath, rightDecoImagePath } from "../../data/constants";
 
 export default function Home() {
     const logoImagePath = "/images/brand/large-logo.png";
-    const leftDecoImagePath = "/images/brand/left-deco-leaf.png";
-    const rightDecoImagePath = "/images/brand/right-deco-leaf.png";
-    const userName = "Tiffany";
 
     return (
-        <main className="home">
-            <img src={logoImagePath} alt={altFromFileName(logoImagePath)} className="large-logo" />
-            <div className="welcome-row">
+        <main id="home">
+            <img src={logoImagePath} alt={altFromFileName(logoImagePath)} className="large-page-image" />
+            <div className="page-title-row">
                 <img src={leftDecoImagePath} alt={altFromFileName(leftDecoImagePath)} className="end-deco-image" />
-                <h1 className="home-title">Welcome back, {userName}!</h1>
+                <h1 className="title">Welcome back, {userName}!</h1>
                 <img src={rightDecoImagePath} alt={altFromFileName(rightDecoImagePath)} className="end-deco-image" />
             </div>
             <h2>Track your plants, save their stories, and watch them grow over time.</h2>
