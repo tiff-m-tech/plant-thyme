4927a5240c1cc9cdc344846c26aebf2a83da6be1
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-05 11:27:58 -0400

    Add Current Collection page with empty state
    - Add Current Collection page and route
    - Update page image for Current Collection
    - Update styles

--- PlantThyme/src/App.jsx
+++ PlantThyme/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
+import CurrentCollection from "./components/pages/CurrentCollection";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/currentCollection" element={<CurrentCollection />} />
             </Routes>
             <Footer />
         </>
---
+++ PlantThyme/src/components/pages/CurrentCollection.jsx
@@ -0,0 +1,27 @@
+import { altFromFileName } from "../../utils/altFromFileName";
+import { leftDecoImagePath, rightDecoImagePath } from "../../data/constants";
+import Button from "../layout/Button";
+import { useNavigate } from "react-router";
+
+export default function CurrentCollection() {
+    const currentCollectionImagePath = "/images/brand/current-collection.png";
+    const plantCount = null;
+    const navigate = useNavigate();
+
+    return (
+        <main id="currentCollection">
+            <img src={currentCollectionImagePath} alt={altFromFileName(currentCollectionImagePath)} className="large-page-image" />
+            <div className="page-title-row">
+                <img src={leftDecoImagePath} alt={altFromFileName(leftDecoImagePath)} className="end-deco-image" />
+                <h1 className="title">Current Plant Collection</h1>
+                <img src={rightDecoImagePath} alt={altFromFileName(rightDecoImagePath)} className="end-deco-image" />
+            </div>
+            {plantCount ? (
+                <div>Plant Count: {plantCount}</div>
+            ) : (
+                <p>You have not added any plants to your collection yet. Lets fill this list up with leafy friends! :D</p>
+            )}
+            <Button innerText="Add Plant" onClick={() => navigate("/currentCollection/add")} />
+        </main>
+    );
+}
