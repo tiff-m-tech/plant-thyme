1d73697e253c84a08849d1211bdb60376e18ca00
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-08 15:02:48 -0400

    Add progress picture upload with image preview.

--- PlantThyme/src/components/cards/ProgressGallery.jsx
+++ PlantThyme/src/components/cards/ProgressGallery.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import PageTitle from "../layout/PageTitle";
 import ProgressPictureCard from "./ProgressPictureCard";
 
+// https://www.youtube.com/shorts/bTll3osO074 tutorial for image preview
+
 export default function ProgressGallery({ plant }) {
     const pictures = plant.progressPictures;
-    console.log(pictures);
+    const [selectedImage, setSelectedImage] = useState("");
+    const today = new Date().toLocaleDateString("en-US");
+
     return (
         <section id="progressGallery">
             <PageTitle title="Progress Pictures" />
-            {[...pictures].map((plant) => (
-                <ProgressPictureCard key={plant.id} imgPath={plant.picture} date={plant.date} />
+            <div>
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={(event) => {
+                        const file = event.target.files?.[0];
+                        setSelectedImage(file ? URL.createObjectURL(file) : undefined);
+                    }}
+                />
+            </div>
+            {pictures.map((plant) => (
+                <ProgressPictureCard
+                    key={plant.id}
+                    imgPath={plant.picture}
+                    date={plant.date}
+                    src={`/images/progressPictures/${plant.picture}`}
+                />
             ))}
+            {selectedImage && <ProgressPictureCard src={selectedImage} date={today} />}
         </section>
     );
 }
