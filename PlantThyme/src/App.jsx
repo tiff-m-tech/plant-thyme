ec661c7acc91db2482235c15959c6787a8f25dbf
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-09 15:07:52 -0400

    Split CSS into separate files by component area.

--- PlantThyme/src/App.jsx
+++ PlantThyme/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
 import { currentCollection } from "./data/currentCollection";
-import "./App.css";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
