b94524edcb45de128a8b28bf8722f7e2463a2dc6
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-09 11:33:50 -0400

    Clean up comments in SearchCard.

--- PlantThyme/src/components/cards/SearchCard.jsx
+++ PlantThyme/src/components/cards/SearchCard.jsx
@@ -3,8 +3,7 @@ import { altFromFileName } from "../../utils/altFromFileName";
 import { useNavigate } from "react-router";
 
 export default function SearchCard({ imgPath, name, plant, addPlantToCollection }) {
-    // useNavigate returns a function that lets you navigate programmatically in the browser in response to user interactions or effects
-    // must initialize the hook at the top level to use
+    // Must initialize the hook at the top level to use.
     const navigate = useNavigate();
 
     return (
