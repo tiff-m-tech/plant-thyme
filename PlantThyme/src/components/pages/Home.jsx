dbb48ab8e7e25d1589d9772513f1d8bb04a698d5
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-09 11:23:38 -0400

    Put more info on the Home Page to fill it up a bit.

--- PlantThyme/src/components/pages/Home.jsx
+++ PlantThyme/src/components/pages/Home.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router";
 import { userName } from "../../data/constants";
 import { altFromFileName } from "../../utils/altFromFileName";
 import PageTitle from "../layout/PageTitle";
+import Button from "../layout/Button";
 
 export default function Home() {
     const logoImagePath = "/images/brand/large-logo.webp";
+    const navigate = useNavigate();
 
     return (
         <main id="home">
@@ -15,10 +18,22 @@ export default function Home() {
             <PageTitle title={`Welcome back, ${userName}!`} />
             <h2>Grow your collection, one plant story at a time.</h2>
             <p>
-                Plant Thyme helps you keep track of your plants, save meaningful details like where
-                and when they were purchased, add other notes, and look back on progress photos as
-                they grow.
+                Every plant has a little history, from the day you brought it home to the first new
+                leaf you noticed. Plant Thyme gives those memories a place to grow.
             </p>
+            <div>
+                <h3>🌿 Keep Tabs on Your Leaves</h3>
+                Track all your leafy friends in one cozy place.
+            </div>
+            <div>
+                <h3>📝 Jot Down the Dirt</h3>
+                Save notes, purchase details, and plant memories.
+            </div>
+            <div>
+                <h3>📸 See How They’ve Grown</h3>
+                Look back on progress photos over time.
+            </div>
+            <Button innerText="View My Collection" onClick={() => navigate("/currentCollection")} />
         </main>
     );
 }
