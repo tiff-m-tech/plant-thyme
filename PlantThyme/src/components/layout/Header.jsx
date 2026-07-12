460291d45586a387a41b8a8b4bf1d0a2420eaf6c
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-12 16:48:03 -0400

    Make logo in navbar linked to home page.

--- PlantThyme/src/components/layout/Header.jsx
+++ PlantThyme/src/components/layout/Header.jsx
@@ -1,14 +1,18 @@
 import NavBar from "./NavBar";
+import { Link } from "react-router";
 
 export default function Header() {
     return (
         <header>
             <div className="header-left">
-                <img
-                    src="/images/brand/mini-logo.png"
-                    alt="Mini Plant Thyme Logo"
-                    className="mini-logo"
-                />
+                <Link to="/">
+                    <img
+                        href="index.html"
+                        src="/images/brand/mini-logo.png"
+                        alt="Mini Plant Thyme Logo"
+                        className="mini-logo"
+                    />
+                </Link>
                 <span className="header-brand-name">Plant Thyme</span>
             </div>
             <NavBar />
