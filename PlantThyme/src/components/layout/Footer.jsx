5f3931ba93d0f31ba19787f74b06ca937d73a940
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-13 08:19:56 -0400

    Add font awesome icons to footer.

--- PlantThyme/src/components/layout/Footer.jsx
+++ PlantThyme/src/components/layout/Footer.jsx
@@ -1,7 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
     return (
         <footer>
-            <span>© {new Date().getFullYear()} Plant Thyme</span>
+            <span>© {new Date().getFullYear()} Plant Thyme</span> |
+            <div className="social-links">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebook} className="facebook-icon" />
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} className="twitter-icon" />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} className="instagram-icon" />
+                </a>
+            </div>
         </footer>
     );
 }
