ea0d49b542c446641686cac82e03522d50337022
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-07-19 15:28:19 -0400

    Add or fix comments (typos, explain better).

--- PlantThyme/src/components/cards/ProgressPictureCard.jsx
+++ PlantThyme/src/components/cards/ProgressPictureCard.jsx
@@ -1,7 +1,6 @@
 import { altFromFileName } from "../../utils/altFromFileName";
 
-// Default for fileName is used by newly selected preview images that do not pass a filename yet.
-// Will replace in Unit 2 when backend is added.
+// Default for fileName is used by newly selected preview images that do not pass a filename yet. Will replace in Unit 2 when backend is added.
 export default function ProgressPictureCard({ src, date, fileName = "Progress Picture" }) {
     return (
         <div className="progress-picture-card">
